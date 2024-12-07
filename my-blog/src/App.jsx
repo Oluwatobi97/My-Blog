9fa2322d0a2e2a22bb45f2c48a09918d3e34878c
Oluwatobi97 <victortobi2000@gamil.com>
2024-12-07 21:54:04 +0100

    changed the padding to 0

--- my-blog/src/App.jsx
+++ my-blog/src/App.jsx
@@ -10,7 +10,7 @@ import About from "./Page/About";
 function App() {
   return (
     <>
-      <div className="p-[2px] md:p-[2px] bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90%">
+      <div className="p-[0] md:p-[0] bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90%">
         {/*Header */}
         <Header />
         <Routes>
